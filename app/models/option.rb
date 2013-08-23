371720acfe3a59b507e3ea0ac059a2bc659c65ee
Joshua Lee <jsungholee@gmail.com>
2013-08-23 17:22:10 -0400

    results do not display for nonvoted items

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,12 +2,23 @@ class Option < ActiveRecord::Base
   attr_accessible :choice
 
   def self.number_of(name_of_choice)
-    choice_hash = hash_of_result
-
-    return choice_hash[name_of_choice]
+    @choice_hash = hash_of_result
+    all_of_the_above_counter
+    return @choice_hash[name_of_choice]
   end
 
   def self.hash_of_result
     Option.select(:choice).group(:choice).count
+  end
+
+  def self.all_of_the_above_counter
+    @choice_hash["all_of_the_above"] ||= 0
+    for counter in 0...@choice_hash["all_of_the_above"]
+      ["poll", "slideshow", "countdown", "this_or_that"].each do |choice|
+        @choice_hash[choice] ||= 0
+        @choice_hash[choice] += 1
+      end
+    end
+    @choice_hash
   end
 end
--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -13,4 +13,13 @@ describe Option do
     expect(Option.number_of("poll")).to eql(1)
     expect(Option.number_of("countdown")).to eql(nil)
   end
+
+  it 'adds one to each item if all of the above is picked' do
+    Option.create(choice: "all_of_the_above")
+
+    expect(Option.number_of("poll")). to eql(1)
+    expect(Option.number_of("slideshow")). to eql(1)
+    expect(Option.number_of("countdown")). to eql(1)
+    expect(Option.number_of("this_or_that")). to eql(1)
+  end
 end
